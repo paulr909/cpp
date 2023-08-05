65f4ab1e621d5d42db81ec96e83536ef70a03cb7
paulrogers <paulr909@proton.me>
2023-08-05 13:46:34 +0100

    add c++

--- c++/Ex_Files_CPlusPlus_EssT/Exercise Files/Chap02/hello.cpp
+++
@@ -1,9 +0,0 @@
-// hello.cpp by Bill Weinman <http://bw.org/>
-#include <cstdio>
-using namespace std;
-
-int main()
-{
-    puts("Hello, World!");
-    return 0;
-}
--- c++/Ex_Files_CPlusPlus_EssT/Exercise Files/Chap05/func.h
+++
@@ -1,8 +0,0 @@
-// func.h by Bill Weinman <http://bw.org/>
-
-#ifndef FUNC_H_
-#define FUNC_H_
-
-void func();
-
-#endif // FUNC_H_
