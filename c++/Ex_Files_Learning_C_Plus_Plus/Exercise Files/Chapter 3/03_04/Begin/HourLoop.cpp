cbc0c4e1d66b2e71ca32db5b2af915a67a8b1c45
paulrogers <paulr909@proton.me>
2023-08-05 13:39:03 +0100

    add c++

---
+++ c++/Ex_Files_Learning_C_Plus_Plus/Exercise Files/Chapter 3/03_04/Begin/HourLoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    for (int h = 0; h < 24; h++) {
+        cout << h << ":00\n";
+    }
+
+}
---
+++ c++/Ex_Files_Learning_C_Plus_Plus/Exercise Files/Chapter 6/06_01/end/structExample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+struct students {
+    string name;
+    float GPA;
+};
+
+int main() {
+
+}
